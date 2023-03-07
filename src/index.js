ac842bde708974d79262a0a2f3dffb6be44be42c
Anup <anup@example.com>
2023-03-07 23:03:42 +0530

    Try some stuff(Routers)

--- src/index.js
+++ src/index.js
@@ -2,13 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './App';
+import { HashRouter as Router } from "react-router-dom";
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <Router>
   <React.StrictMode>
     <App />
   </React.StrictMode>
+  </Router>
 );
 
 // If you want to start measuring performance in your app, pass a function
