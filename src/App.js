c4ac51926fd644b1c433eb140a79b6afad1acb65
Anup <anup@example.com>
2023-03-08 12:52:49 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 
 import './App.css';
-
+import { useState } from "react";
 
 function App() {
+  const[addTask,setAddTask] = useState(0);
+  const[newTask,setNewTask] = useState("");
   return (
   <div className="App"> 
-     <div>
-      Hii 
+     <div className="addTask">
+      <input type="text" />
+      <button>Add Task</button>
      </div>
+     <div className="list"></div>
     </div>
   );
 }
