b743945726ad74989774b708a57d8f098c2b2646
Anup <anup@example.com>
2023-03-14 08:28:49 +0530

    New delete and add feature added.

--- src/App.js
+++ src/App.js
@@ -3,15 +3,47 @@ import './App.css';
 import { useState } from "react";
 
 function App() {
-  const[addTask,setAddTask] = useState(0);
+  const[todolist,setTodolist] = useState([]);
   const[newTask,setNewTask] = useState("");
+
+   const handleChange= (event)=>{
+      setNewTask(event.target.value);
+   }
+
+   const addNewTask = () =>{
+     setTodolist([...todolist,newTask]);
+   }
+
+   const deleteTask = (taskName) =>{
+    const newTodoList1 = todolist.filter((task)=>{
+      if(task === taskName){
+        return false;
+      }else{
+        return true;
+      }
+      
+    }); setTodolist(newTodoList1);
+
+   }
+
   return (
   <div className="App"> 
      <div className="addTask">
-      <input type="text" />
-      <button>Add Task</button>
+      <input type="text" onChange={handleChange} />
+      <button onClick={addNewTask}>Add Task</button>
      </div>
-     <div className="list"></div>
+     <div className="list">
+      {todolist.map((task)=>{
+        return (
+          <div>
+        <h1>{task}</h1>
+        <button onClick={()=> deleteTask(task)}>X</button>
+        </div>
+        );
+      }
+      )}
+     </div>
+     
     </div>
   );
 }
